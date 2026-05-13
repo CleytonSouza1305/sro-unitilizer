d4ee219ece528825c5dfe68bb46d5498a669718d
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-05-12 22:49:09 -0300

    Creating table User on database

---
+++ prisma/migrations/20260513010653_creating_user_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('OWNER', 'ADMIN', 'MANAGER', 'USER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" VARCHAR(255) NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" "UserRole" NOT NULL DEFAULT 'USER',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_id_key" ON "User"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
