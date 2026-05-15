b35b2cc86b4f5ca5ba9cf25fc6114fbd708c79eb
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-05-15 16:01:25 -0300

    feat: implement User interface and createUser method in User model

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,7 +1,18 @@
 import { prisma } from "../database"
+import { UserRole } from "../generated/prisma/enums"
 
+interface UserInterface {
+  username: string
+  email: string,
+  password: string,
+  role?: UserRole
+}
 export default class User {
   static getByEmail = async (email:string) => {
     return await prisma.user.findUnique({ where: { email } })
+  }
+
+  static createUser = async (data: UserInterface) => {
+    console.log(data)
   }
 }
