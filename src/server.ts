ac41952e40f78ce61a4373554358f5bce9daaf7c
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-05-15 16:00:57 -0300

    feat: add authentication route to the API

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import { errorMiddleware } from './middleware/error-handler'
 import puppeteerRouter from './routes/puppeteer-router'
+import authRouter from "./routes/auth-router";
 config()
 
 const app = express()
@@ -11,6 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api', puppeteerRouter)
+app.use('/api/auth', authRouter)
 
 app.use(errorMiddleware)
 
