e35b3b0adfc873c1dc6cb38bb3eac4691c7dd313
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-05-21 18:06:04 -0300

    refactor(user): format code and improve getByEmail method structure

--- src/model/User.ts
+++ src/model/User.ts
@@ -1,29 +1,31 @@
-import { prisma } from "../database"
-import { UserRole } from "../generated/prisma/enums"
+import { prisma } from "../database";
+import { UserRole } from "../generated/prisma/enums";
 
 export interface UserInterface {
-  username: string
-  email: string,
-  password: string,
-  role?: UserRole
+  username: string;
+  email: string;
+  password: string;
+  role?: UserRole;
 }
 export default class User {
-  static getByEmail = async (email:string) => {
-    return await prisma.user.findUnique({ where: { email } })
-  }
+  static getByEmail = async (email: string) => {
+    return await prisma.user.findUnique({
+      where: { email },
+      select: { email: true, id: true, role: true, password: true },
+    });
+  };
 
   static createUser = async (data: UserInterface) => {
-    return data
-  }
+    return data;
+  };
 
   static findById = async (id: string) => {
-    return await prisma.user.findUnique({ 
-      where: 
-      { id }, 
-      include: { 
-        created_by_user: true, 
-        created_users: true 
-      }
-    })
-  }
+    return await prisma.user.findUnique({
+      where: { id },
+      include: {
+        created_by_user: true,
+        created_users: true,
+      },
+    });
+  };
 }
