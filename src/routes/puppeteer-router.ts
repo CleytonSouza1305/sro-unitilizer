92a51d6b300edef834708be46f77ecc7f42b02b4
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-05-21 18:06:24 -0300

    feat(puppeteer): add authorization middleware to scrapp route

--- src/routes/puppeteer-router.ts
+++ src/routes/puppeteer-router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { getUnitilizer } from '../controllers/scrapper-controller'
+import { authorizationByToken } from '../middleware/authorization-middleware'
 
 const puppeteerRouter = Router()
 
-puppeteerRouter.get('/scrapp', getUnitilizer)
+puppeteerRouter.get('/scrapp', authorizationByToken, getUnitilizer)
 
 export default puppeteerRouter
