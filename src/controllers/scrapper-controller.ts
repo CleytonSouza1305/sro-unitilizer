8363803a1d6e9a142c41961efd2a68fb6950c3a5
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-05-31 20:17:07 -0300

    feat(scrapper): implement closeUnitilizer function to handle unit closure logic

--- src/controllers/scrapper-controller.ts
+++ src/controllers/scrapper-controller.ts
@@ -1,6 +1,6 @@
 import { config } from "dotenv";
 import { RequestHandler } from "express";
-import { puppeteerService } from "../services/PuppeteerService";
+import { puppeteerService, Unitizer } from "../services/PuppeteerService";
 import { HttpError } from "../error/HttpError";
 config();
 
@@ -45,4 +45,131 @@ const getUnitilizer: RequestHandler = async (req, res, next) => {
   }
 };
 
-export { getUnitilizer };
+const closeUnitilizer: RequestHandler = async (req, res, next) => {
+  try {
+    const { unitilizers } = req.body;
+
+    if (!url) {
+      throw new HttpError(
+        "MISSING_URL",
+        400,
+        "A URL de destino é obrigatória.",
+      );
+    }
+
+    if (
+      !unitilizers ||
+      !Array.isArray(unitilizers) ||
+      unitilizers.length === 0
+    ) {
+      throw new HttpError(
+        "MISSING_DATA",
+        400,
+        "É necessário fornecer um array de unitizadores.",
+      );
+    }
+
+    const user = process.env.PUPPETEER_USER;
+    const pass = process.env.PUPPETEER_PASS;
+
+    if (!user || !pass) {
+      throw new HttpError(
+        "INTERNAL_CONFIG_ERROR",
+        500,
+        "Credenciais do serviço não configuradas no ambiente.",
+      );
+    }
+
+    const isConnected = await puppeteerService.connectAndLogin(url, user, pass);
+
+    if (!isConnected) {
+      throw new HttpError(
+        "PUPPETEER_AUTH_FAILED",
+        401,
+        "Não foi possível autenticar no serviço de expedição.",
+      );
+    }
+
+    const data = await puppeteerService.getUnitilizer(url);
+
+    const results: {
+      closeds: Unitizer[];
+      error: { unit: string; reason: string }[];
+    } = {
+      closeds: [],
+      error: [],
+    };
+
+    for (let i = 0; i < unitilizers.length; i++) {
+      const currentUnit = unitilizers[i];
+
+      const isValidUnitilizer = data.find(
+        (el) => el.unitilizer === currentUnit,
+      );
+      if (!isValidUnitilizer) {
+        results.error.push({
+          unit: currentUnit,
+          reason: "Não encontrado na base de destino",
+        });
+        continue;
+      }
+
+      if (
+        +isValidUnitilizer.number === 17 ||
+        isValidUnitilizer.destination.toLowerCase().includes("cajamar")
+      ) {
+        const date = new Date();
+        date.setHours(0, 0, 0, 0);
+
+        const dateString = isValidUnitilizer.date.split("-")[0].trim();
+        const [day, month, year] = dateString.split("/");
+
+        const unitilizerDate = new Date(+year, +month - 1, +day);
+        if (date > unitilizerDate) {
+          const wasClosed = await puppeteerService.closeUnitilizer(
+            url,
+            isValidUnitilizer.unitilizer,
+          );
+
+          if (wasClosed?.success) {
+            results.closeds.push(isValidUnitilizer);
+            continue;
+          } else {
+            results.error.push({
+              unit: currentUnit,
+              reason: "O serviço do robô não retornou confirmação de sucesso.",
+            });
+          }
+        } else {
+          results.error.push({
+            unit: currentUnit,
+            reason: `Bloqueado: Não é permitido fechar "${isValidUnitilizer.destination}" no mesmo dia da abertura.`,
+          });
+        }
+
+        continue;
+      }
+
+      const wasClosed = await puppeteerService.closeUnitilizer(
+        url,
+        isValidUnitilizer.unitilizer,
+      );
+
+      if (wasClosed?.success) {
+        results.closeds.push(isValidUnitilizer);
+        continue;
+      } else {
+        results.error.push({
+          unit: currentUnit,
+          reason: "O serviço do robô não retornou confirmação de sucesso.",
+        });
+      }
+    }
+
+    res.json({ results });
+  } catch (e) {
+    next(e);
+  }
+};
+
+export { getUnitilizer, closeUnitilizer };
