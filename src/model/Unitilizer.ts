dc2e207dbbddc51aac325ae4e498f8bdc01b902e
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-06-05 15:09:14 -0300

    feat: implement closeUnit method in Unitilizer model

---
+++ src/model/Unitilizer.ts
@@ -0,0 +1,34 @@
+import { prisma } from "../database";
+
+interface CloseUnitInput {
+  number: number;
+  unitilizer: string;
+  destination: string;
+  date: string;
+  closedByUserId: string;
+  objects: { data: string[]; quantity: number };       
+}
+
+export default class Unitilizer {
+  static closeUnit = async (data: CloseUnitInput, userId: string) => {
+
+    const unitilizer = await prisma.unitilizer.create({ 
+      data: {
+        date: data.date,
+        destination: data.destination,
+        number: +data.number,
+        unitilizer: data.unitilizer,
+        closed_by_userId: userId,
+        objects: {
+          createMany: {
+            data: data.objects.data.map((l) => ({
+              label: l
+            }))
+          }
+        }
+      }
+    })
+
+    return unitilizer
+  }
+}
