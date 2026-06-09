1bf90c1e4f5e37996426e2f8a48af5c79c9bc9a6
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-06-08 23:27:22 -0300

    feat: add endpoint to return all objects in puppeteerRouter

--- src/routes/puppeteer-router.ts
+++ src/routes/puppeteer-router.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { closeUnitilizer, getUnitilizer } from '../controllers/scrapper-controller'
+import { allObjects, closeUnitilizer, getUnitilizer } from '../controllers/scrapper-controller'
 import { authorizationByToken } from '../middleware/authorization-middleware'
 
 const puppeteerRouter = Router()
 
 puppeteerRouter.get('/scrapp', authorizationByToken, getUnitilizer)
 puppeteerRouter.post('/scrapp/close', authorizationByToken, closeUnitilizer)
+puppeteerRouter.get('/scrapp/objects', authorizationByToken, allObjects)
 
 export default puppeteerRouter
