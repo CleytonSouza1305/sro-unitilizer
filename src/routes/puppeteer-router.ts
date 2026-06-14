509c6d2f1ede7fe2335f333359937f73fbde42b9
Cleyton de Jesus Conceição Souza <141965244+CleytonSouza1305@users.noreply.github.com>
2026-06-14 17:41:55 -0300

    feat: adicionar rota para obter unitilizers disponíveis no puppeteer-router

--- src/routes/puppeteer-router.ts
+++ src/routes/puppeteer-router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { allObjects, closeUnitilizer, getUnitilizer } from '../controllers/scrapper-controller.js'
+import { allObjects, closeUnitilizer, getAvaliableUnit, getUnitilizer } from '../controllers/scrapper-controller.js'
 import { authorizationByToken } from '../middleware/authorization-middleware.js'
 
 const puppeteerRouter = Router()
@@ -7,5 +7,6 @@ const puppeteerRouter = Router()
 puppeteerRouter.get('/scrapp', authorizationByToken, getUnitilizer)
 puppeteerRouter.post('/scrapp/close', authorizationByToken, closeUnitilizer)
 puppeteerRouter.get('/scrapp/objects', authorizationByToken, allObjects)
+puppeteerRouter.get('/scrapp/unitilizers', authorizationByToken, getAvaliableUnit)
 
 export default puppeteerRouter
